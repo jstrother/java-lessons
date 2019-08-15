98ac94c07ead5ea71b99eac1590f073d5ccfae7e
Jim Strother <jim.strother@gmail.com>
2019-08-15 13:25:36 -0700

    adding java files

---
+++ ConvertPoundsToKilograms/Convert.java
@@ -0,0 +1,7 @@
+public class Convert {
+  public static void main(String[] args) {
+    double pounds = 200d;
+    double kilograms = pounds * 0.45359237d;
+    System.out.println(pounds + "lbs = " + kilograms + "kg");
+  }
+}
