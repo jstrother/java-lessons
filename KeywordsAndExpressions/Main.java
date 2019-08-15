b06d51b2c9cd934ce6be749b3689c7932d717a41
Jim Strother <jim.strother@gmail.com>
2019-08-15 16:11:33 -0700

    some keywords and expressions stuff

---
+++ KeywordsAndExpressions/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    double kilometers = (100 * 1.609344);
+    int highScore = 50;
+    if (highScore == 50) {
+      System.out.println("this is an expression");
+      System.out.println(kilometers);
+    }
+  }
+}
+
+// from video challenge
+// which parts of the following code are expressions?
+
+// int score = 100; only int is not part of the expression, just score = 100
+
+// if (score > 99) { only score > 99 is an expression
+//   System.out.println("You got the high score!"); the string output is the expression, including ""
+//   score = 0; this is an expression, with exception of ;
+// }
