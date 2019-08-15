d300dbf6ec57f98127e14939c76575eb8e90a83f
Jim Strother <jim.strother@gmail.com>
2019-08-15 15:11:54 -0700

    list of operators and a finished challenge added

--- Operators/Operators.java
+++ Operators/Operators.java
@@ -1,5 +1,6 @@
 public class Operators {
   public static void main(String[] args) {
+    // this is a comment. the computer will not read anything behind the double /. it is for human eyes only
     int result = 1 + 2; // 1 + 2 = 3
     System.out.println("1 + 2 = " + result);
     int previousResult = result;
@@ -90,15 +91,90 @@ public class Operators {
 
     topScore = 80;
     int secondTopScore = 60;
-    if (topScore > secondTopScore && topScore < 100) { // && is the logical AND operator which checks to see if both statements are true
+    if ((topScore > secondTopScore) && (topScore < 100)) { // && is the logical AND operator which checks to see if BOTH statements are true
       // this will print
       System.out.println("topScore is greater than secondTopScore and less than 100");
     }
 
     secondTopScore = 81;
-    if (topScore > secondTopScore && topScore < 100) {
+    if ((topScore > secondTopScore) && (topScore < 100)) {
       // this will not print
       System.out.println("secondTopScore is now larger than topScore");
+    }
+
+    if ((topScore > 90) || (secondTopScore > topScore)) { // || is the logical OR operator which checks to see if EITHER or BOTH statement is true, in this case the first is false and the second is true
+      // this will print
+      System.out.println("secondTopScore is might finally be larger than topScore");
+    }
+
+    if ((topScore > 90) || (secondTopScore > 95)) { // both statements are now false
+      // this will not print
+      System.out.println("both statements are false");
+    }
+
+    int newValue = 50;
+    // if (newValue = 50) {
+    //   System.out.println("This is an error! the if statement needs to determine booleans using ==, and cannot reassign a value using =");
+    // }
+
+    if (newValue == 50) { // == is how to determine true/false, = only assigns values to int, double, boolean, etc.
+      // this will print
+      System.out.println("newValue = 50");
+    }
+
+    boolean isCar = false;
+    if (isCar = true) { // boolean checks can reassign with =. Be careful!!
+      System.out.println("This should not print!");
+    }
+    // isCar is now equal to true
+    System.out.println("isCar = " + isCar);
+
+    isCar = false; // resetting isCar because the above code block changed the value
+    if (isCar == true) { // == to compare!
+      System.out.println("This really won't print");
+    }
+
+    if (isCar) { // shortcut for the above check. still looks to see if isCar == true, in this case no
+      System.out.println("Another that won't print'");
+    }
+
+    if (isCar != true) { // remember that != checks for the opposite of something. isCar is NOT equal to true
+      System.out.println("Finally! Displaying!");
+    }
+
+    if (isCar == false) { // evaluates to a true statement, after all isCar is equal to false
+      System.out.println("Another display!");
+    }
+
+    if (!isCar) { // this is a shortcut for the above 2 as ! will check the opposite of what is placed next to. isCar is equal to false, so not false is true
+      System.out.println("Three straight displays! We're on fire!");
+    }
+
+    boolean isAirplane = true;
+    if (!isAirplane) { // since isAirplane is true, this becomes not true, or false.
+      System.out.println("back to not displaying");
+    }
+
+    boolean wasCar = isCar ? true : false;
+    // ternary operator
+    // if isCar is equal to true do the first option
+    // if isCar is equal to false do the second option
+    // this will assign one of those two values to wasCar
+    if (wasCar) { // isCar is equal to false, so wasCar gets assigned the second value of false
+      System.out.println("not true, not displaying");
+    }
+
+    isCar = true;
+    wasCar = isCar ? true : false;
+    if (wasCar) { // now that isCar is true, the first value of true gets assigned to wasCar
+      System.out.println("This prints!");
+    }
+
+    int ageOfClient = 20;
+    boolean isEighteenOrOver = (ageOfClient >= 18) ? true : false;
+    // can also use ==, <=, <, > to compare instead of just >=
+    if (isEighteenOrOver) { // this is true. 20 > 18
+      System.out.println("Over eighteen, this prints");
     }
   }
 }
