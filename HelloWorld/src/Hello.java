fa58a18e8d2e07102572607119610502d801e645
Jim Strother <jim.strother@gmail.com>
2020-03-29 13:14:41 -0700

    Added: simple "hello world" along with basic int with some math in assignment

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello, Jim");
 
-    int myFirstNumber = (10 + 5) + (2 * 10);
+    int myFirstNumber = 10 + 5;
     System.out.println(myFirstNumber);
   }
 }
